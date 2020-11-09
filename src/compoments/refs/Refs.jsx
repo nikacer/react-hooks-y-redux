463f0d0d49f2e23636d94666ed6d72bb0c81de43
Ivan Nicolas Acero García <ivan.acero@avaldigitallabs.com>
2020-11-09 09:09:00 -0500

    generacion de una herencia de un ref

--- src/compoments/refs/Refs.jsx
+++ src/compoments/refs/Refs.jsx
@@ -1,60 +1,66 @@
-import React, { useRef, useEffect, Fragment } from "react";
+import React, { useRef, useEffect, Fragment, forwardRef } from "react";
 import Chart from "chart.js";
 
 function Refs() {
   const inputExample = useRef(null);
 
-  useEffect(() => {
-    const ctx = inputExample.current.getContext("2d");
-
-    new Chart(ctx, {
-      type: "bar",
-      data: {
-        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-        datasets: [
-          {
-            label: "# of Votes",
-            data: [12, 19, 3, 5, 2, 3],
-            backgroundColor: [
-              "rgba(255, 99, 132, 0.2)",
-              "rgba(54, 162, 235, 0.2)",
-              "rgba(255, 206, 86, 0.2)",
-              "rgba(75, 192, 192, 0.2)",
-              "rgba(153, 102, 255, 0.2)",
-              "rgba(255, 159, 64, 0.2)",
-            ],
-            borderColor: [
-              "rgba(255, 99, 132, 1)",
-              "rgba(54, 162, 235, 1)",
-              "rgba(255, 206, 86, 1)",
-              "rgba(75, 192, 192, 1)",
-              "rgba(153, 102, 255, 1)",
-              "rgba(255, 159, 64, 1)",
-            ],
-            borderWidth: 1,
-          },
-        ],
-      },
-      options: {
-        scales: {
-          yAxes: [
-            {
-              ticks: {
-                beginAtZero: true,
-              },
-            },
-          ],
-        },
-      },
-    });
-  }, []);
-
   return (
     <Fragment>
       <h1>Graficas</h1>
-      <canvas id="myChart" width="400" height="400" ref={inputExample}></canvas>
+      <HerenciaRef ref={inputExample} />
     </Fragment>
   );
 }
+
+const HerenciaRef = forwardRef((props, ref) => {
+  useEffect(() => {
+    if (ref.current) {
+      const ctx = ref.current.getContext("2d");
+
+      new Chart(ctx, {
+        type: "bar",
+        data: {
+          labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+          datasets: [
+            {
+              label: "# of Votes",
+              data: [12, 19, 3, 5, 2, 3],
+              backgroundColor: [
+                "rgba(255, 99, 132, 0.2)",
+                "rgba(54, 162, 235, 0.2)",
+                "rgba(255, 206, 86, 0.2)",
+                "rgba(75, 192, 192, 0.2)",
+                "rgba(153, 102, 255, 0.2)",
+                "rgba(255, 159, 64, 0.2)",
+              ],
+              borderColor: [
+                "rgba(255, 99, 132, 1)",
+                "rgba(54, 162, 235, 1)",
+                "rgba(255, 206, 86, 1)",
+                "rgba(75, 192, 192, 1)",
+                "rgba(153, 102, 255, 1)",
+                "rgba(255, 159, 64, 1)",
+              ],
+              borderWidth: 1,
+            },
+          ],
+        },
+        options: {
+          scales: {
+            yAxes: [
+              {
+                ticks: {
+                  beginAtZero: true,
+                },
+              },
+            ],
+          },
+        },
+      });
+    }
+  }, [ref]);
+
+  return <canvas id="myChart" width="400" height="400" ref={ref}></canvas>;
+});
 
 export default Refs;
